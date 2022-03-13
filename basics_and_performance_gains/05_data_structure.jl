f2c0a270387f8e1af421f5212f8c7a2002df624c
PhillNo <24880693+PhillNo@users.noreply.github.com>
2022-03-13 09:15:26 -0700

    Possible incorrect assumption: 3D array implemented as array or array of arrays?
    explanation in comments

--- basics_and_performance_gains/05_data_structure.jl
+++ basics_and_performance_gains/05_data_structure.jl
@@ -21,10 +21,10 @@ resides at.
 The following demonstrates the time to sum elements along a row, column, and layer of a data cube.
 =#
 
-const depth = 1000 # greater cube depth will exaggerate timing difference. Permuting a large cube will take much longer.
+const depth = 10clear00 # greater cube depth will exaggerate timing difference. Permuting a large cube will take much longer.
 
 # TODO: different behavior observed when using Float32 vs Float64
-const data_cube = rand(Float32, (depth, depth, depth))
+@time const data_cube = rand(Int64, (depth, depth, depth)) #since addings Ints is faster this won't dominate element access time as much
 
 println(typeof(data_cube))
 
@@ -35,10 +35,13 @@ print("sum col: ")
 print("sum layer: ")
 @time sum(data_cube[1, 1, :]) # for a larger data cube the relative differences are more exaggerated
 
+println("\nWhy is sum cols sometimes slower than sum layers? A 3-D vector may not be implmeneted as a contiguous chunk of memory with different stride length for accessing row, column, layer elements, as I had assumed. Instead, it might be an array of arrays. This still means accessing elements in the same row is quicker since they will usually be on the same page in virtual memory, but access time in the column or layer direction may be the same.\nSomething to note: Julia arrays are colum-major ordered whereas C++ arrays are row-major ordered.")
+
 # ===========================
 # Uncomment the below to see how long it takes to rotate the data cube (consider decreasing "depth" so you have enough memory)
+
 # const permuted = zeros(Float64, (depth, depth, depth))
 # print("time to permute a 500x500x500 data cube: ")
 # @time permutedims!(permuted, data_cube, [3, 2, 1]) #in-place permutation not supported
-# 
+#
 # println("original row sum equals permuted page sum: ", sum(data_cube[:, 1, 1]) == sum(permuted[1, 1, :]))
